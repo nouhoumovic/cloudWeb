ab1fbcb2733bb499b8c94b7ce09a38336b8772e1
Nouhou <nmaiga4@gmail.com>
2021-08-23 18:53:49 +0000

    first commit

---
+++ group1.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <title>TP5</title>
+        <meta charset="UTF-8">
+       
+    </head>
+    <body>
+        <div>
+<p> <B> <U> Nom du Groupe</U> GROUPE 1</B></p>
+<p> <B> <U> Classe:</U> GL5  Cloud Computing</B></p>
+<p> <B> <U> TP5#:</U> Server Web</B></p>
+</div>
+    </body>
+</html>
